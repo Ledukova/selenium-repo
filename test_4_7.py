e713be40a10a30992d4728a04f26ef78eeef36a3
Ольга Ледюкова <91422217+Ledukova@users.noreply.github.com>
2022-02-22 00:23:17 +0300

    Add files via upload
    Задание 7. Сделайте сценарий, проверяющий наличие стикеров у товаров (попытка 2)

--- test_4_7.py
+++ test_4_7.py
@@ -16,15 +16,15 @@ def test_example(driver):
     hi=0 # кол-во без стикера
     hj=0 # кол-во более одного стикера
     for i in range(0, len(my_list)):
-        sticker_list=my_list[i].find_elements_by_css_selector("[class^='sticker']")
+        sticker_list=my_list[i].find_elements_by_css_selector(".sticker")
         if len(sticker_list)==0:
-            print('Продукт ', i, ' без стикер')
+            print('Продукт ', i+1, ' без стикер')
             hi=hi+1
         if len(sticker_list)>1:
-            print('У продукта ', i, ' более одного стикера')
+            print('У продукта ', i+1, ' более одного стикера')
             hj=hj+1
     if (hi>0) or (hj>0):
-        raise Exception(hi, ' продуктов без стикеров. У ', hj, ' продуктов более одного стикера')
+        raise Exception(hi, "продуктов без стикеров. У", hj, "продуктов более одного стикера")
 
 
 
