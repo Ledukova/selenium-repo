fab26fca3f06d32766d332fee187d0a11e698992
Ольга Ледюкова <91422217+Ledukova@users.noreply.github.com>
2022-03-06 23:01:37 +0300

    Add files via upload
    Задание 13. Сделайте сценарий работы с корзиной (попытка 3)

--- 7-13-2.py
+++ 7-13-2.py
@@ -43,23 +43,16 @@ def test_example(driver):
     driver.find_element_by_css_selector("div#cart .link").click()  # переход в корзину
     tovarov_v_korzine = driver.find_elements_by_name('remove_cart_item')
     #print("товаров в корзине", len(tovarov_v_korzine))
-    l = len(tovarov_v_korzine)
-    while (len(tovarov_v_korzine) > 0) and (l > 0):
+    while (len(tovarov_v_korzine) > 0):
         #print("l на входе", l)
-        wait.until(EC.visibility_of(tovarov_v_korzine[0]))  # ждем когда элемент станет видимым
+        t = driver.find_elements_by_css_selector(".footer strong")[1]
         tovarov_v_korzine[0].click()  # удаляем из корзины товар начиная с последнего
-        t = driver.find_elements_by_css_selector(".footer strong")[1].get_attribute("textContent")
-        #print("t", t)
-        t_new = t
-        if l > 1:
-            while price_v_float(t) == price_v_float(t_new):
-                wait.until(EC.staleness_of(driver.find_elements_by_css_selector(".footer strong")[1]))
-                wait.until(lambda d: d.find_elements_by_css_selector(".footer strong")[1])  # ждем появления таблицы
-                t_new = driver.find_elements_by_css_selector(".footer strong")[1].get_attribute("textContent")
-                #print("t_new", t_new)
-        l = l-1
-        tovarov_v_korzine = driver.find_elements_by_name('remove_cart_item')  # обновляем список товаров в корзине
-        #print("товаров в корзине", len(tovarov_v_korzine))
+        print("t ", t.get_attribute("textContent"))
+        wait.until(EC.staleness_of(t))  # ждем исчезновение элемента в таблице
+        tovarov_v_korzine = driver.find_elements_by_name('remove_cart_item')
+        print(len(tovarov_v_korzine), "товаров в корзине")
+        if len(tovarov_v_korzine) > 0:
+            wait.until(lambda d: d.find_elements_by_css_selector(".footer strong")[1])  # ждем появления элемента в таблице
     if len(tovarov_v_korzine) == 0:
         print("Товара в корзине нет")
     else:
